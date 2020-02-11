04f15ec05a5038184c40e520bb6a9e0b1965ea01
Xie Huang <huangcrab@gmail.com>
2020-02-10 23:51:27 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/reef-data/precache-manifest.88be66e7301cac2f4005d4bf0a37a43b.js"
+  "/precache-manifest.54dc6b1637a3936f0abf315579bc6ae5.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/reef-data/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
