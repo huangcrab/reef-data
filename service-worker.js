7e186c931dbc7959daaec91ae3410d7e3ccb1c5b
Xie Huang <huangcrab@gmail.com>
2020-02-11 00:14:10 -0500

    add mag

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.54dc6b1637a3936f0abf315579bc6ae5.js"
+  "/reef-data/precache-manifest.86e6377b1e15a74e83107a0ff5d79c53.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/reef-data/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
