ff866c69c1272ff850afb8cf0c86e32f0ee0d98b
x <kirinreef@gmail.com>
2023-02-04 23:09:22 -0500

    open the dashboard to public

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import React, { Component } from "react";
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import React, { Component } from 'react';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 
-import { UserIsAuthenticated, UserIsNotAuthenticated } from "./helpers/auth";
+import { UserIsAuthenticated, UserIsNotAuthenticated } from './helpers/auth';
 
-import Navbar from "./component/layout/Navbar";
-import Dashboard from "./component/layout/Dashboard";
-import Login from "./component/auth/Login";
-import AddTest from "./component/tests/AddTest";
-import EditTest from "./component/tests/EditTest";
-import TestResult from "./component/tests/TestDetail";
-import Settings from "./component/user/Settings";
-import Register from "./component/auth/Register";
-import { Provider } from "react-redux";
-import store from "./store";
+import Navbar from './component/layout/Navbar';
+import Dashboard from './component/layout/Dashboard';
+import Login from './component/auth/Login';
+import AddTest from './component/tests/AddTest';
+import EditTest from './component/tests/EditTest';
+import TestResult from './component/tests/TestDetail';
+import Settings from './component/user/Settings';
+import Register from './component/auth/Register';
+import { Provider } from 'react-redux';
+import store from './store';
 
-import "./App.css";
+import './App.css';
 
 class App extends Component {
   render() {
@@ -25,11 +25,7 @@ class App extends Component {
             <Navbar />
             <div className="container">
               <Switch>
-                <Route
-                  exact
-                  path="/"
-                  component={UserIsAuthenticated(Dashboard)}
-                />
+                <Route exact path="/" component={Dashboard} />
                 <Route
                   exact
                   path="/test/add"
